9773ce819ab7797fa1cebb8c0d34f239cdd13e4d
Gustavo <whoisgustavolima@gmail.com>
2025-02-24 00:03:57 -0300

    nickinho

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -7,7 +7,7 @@ export function Message({children,from}:props) {
 
     if (from === 'bot') {
         return (
-            <div className="p-4 text-indigo-500 bg-indigo-300 rounded-xl w-max max-w-[55%]">
+            <div className="p-4 text-indigo-500 bg-indigo-300 rounded-xl w-max max-w-[70%]">
                 {children}
             </div>
         )
@@ -15,7 +15,7 @@ export function Message({children,from}:props) {
 
     if (from === 'human') {
         return (
-            <div className="p-4 text-indigo-200 bg-indigo-400 rounded-xl w-max max-w-[55%] ms-auto">
+            <div className="p-4 text-indigo-200 bg-indigo-400 rounded-xl w-max max-w-[70%] ms-auto">
                 {children}
             </div>
         )
